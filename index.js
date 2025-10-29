4624433687e78845801cd559dcb184a22a0e14fc
jasonsolar777 <jasonsolar777@gmail.com>
2025-10-28 20:42:41 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') return res.end('ok');
+  res.statusCode = 404;
+  res.end('not found');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Listening on", PORT));
